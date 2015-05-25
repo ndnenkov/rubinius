76e434c4e91b4db423945006c11cb72117fc30e5
Brian Shirai <brixen@gmail.com>
2015-05-25 14:43:44 -0500

    Fixed default-in-covered-case warning.
    This was seen on Travis CI OS X environment:
    
    2: CXX vm/builtin/encoding.cpp
    
    vm/builtin/encoding.cpp:729:5: error: default label in switch which covers all enumeration values [-Werror,-Wcovered-switch-default]
    
        default:

--- vm/builtin/encoding.cpp
+++ vm/builtin/encoding.cpp
@@ -727,9 +727,9 @@ namespace rubinius {
       return state->symbol("after_output");
     case econv_incomplete_input:
       return state->symbol("incomplete_input");
-    default:
-      return state->symbol("unknown_converter_error");
     }
+
+    return state->symbol("unknown_converter_error");
   }
 
   Symbol* Converter::primitive_convert(STATE, Object* source, String* target,
